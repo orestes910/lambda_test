64774204568a7edcdbb2adf2b8e0c97e07de4d2a
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-12 14:44:42 -0600

    Fixed request

--- main.go
+++ main.go
@@ -2,21 +2,61 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 type Weather struct {
-	current string
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp     float64 `json:"temp"`
+		Pressure int     `json:"pressure"`
+		Humidity int     `json:"humidity"`
+		TempMin  float64 `json:"temp_min"`
+		TempMax  float64 `json:"temp_max"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int     `json:"type"`
+		ID      int     `json:"id"`
+		Message float64 `json:"message"`
+		Country string  `json:"country"`
+		Sunrise int     `json:"sunrise"`
+		Sunset  int     `json:"sunset"`
+	} `json:"sys"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Cod  int    `json:"cod"`
 }
 
 func main() {
-	resp, respErr := http.Get("api.openweathermap.org/data/2.5/weather?zip=80401&APPID=699d450b072c35858db90d31b95e0fc0")
+	resp, respErr := http.Get("http://api.openweathermap.org/data/2.5/weather?zip=80401&APPID=699d450b072c35858db90d31b95e0fc0")
 	if respErr != nil {
 		log.Fatal("Request failed")
 	}
 	data, _ := ioutil.ReadAll(resp.Body)
 	var jsonData Weather
 	json.Unmarshal(data, &jsonData)
+
+	fmt.Println("Location: ", jsonData.Name)
 }
