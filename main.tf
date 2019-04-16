454973a36618a5bf7162f2e87ab027a2718b6155
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-16 11:32:00 -0600

    Initial deploy test

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region    = "us-east-2a"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "weather" {
+  filename         = "/tmp/weather.zip"
+  function_name    = "weather"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "weather"
+  source_code_hash = "${filebase64sha256("/tmp/weather.zip")}"
+  runtime          = "go1.x"
+}
