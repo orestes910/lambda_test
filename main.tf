79d9f354f28df8343aa49aa1ca54e2248981ddf6
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-22 15:07:41 -0600

    Added usage plan to attach API key

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#AWS
 provider "aws" {
   region    = "us-east-2"
 }
@@ -11,6 +12,57 @@ terraform {
     }
 }
 
+#API and Usage Plan
+resource "aws_api_gateway_rest_api" "weather_api" {
+  name = "weather"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part = "resource"
+  parent_id = "${aws_api_gateway_rest_api.weather_api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.weather_api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id = "${aws_api_gateway_rest_api.weather_api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "GET"
+  authorization = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = "${aws_api_gateway_rest_api.weather_api.id}"
+  stage_name = "stage-1"
+}
+
+resource "aws_api_gateway_usage_plan" "weather_usage_plan" {
+  name = "weather-usage"
+  product_code = "weather"
+  api_stages {
+    api_id = "${aws_api_gateway_rest_api.weather_api.id}"
+    stage = "${aws_api_gateway_deployment.deployment.stage_name}"
+  }
+  quota_settings {
+    limit = 20
+    offset = 2
+    period = "WEEK"
+  }
+  throttle_settings {
+    burst_limit = 5
+    rate_limit = 10
+  }
+}
+
+resource "aws_api_gateway_api_key" "weather_key" {
+  name = "weather"
+}
+
+resource "aws_api_gateway_usage_plan_key" "weather_plan_key" {
+  key_id = "${aws_api_gateway_api_key.weather_key.id}"
+  key_type = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.weather_usage_plan}"
+}
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
@@ -30,6 +82,14 @@ resource "aws_iam_role" "iam_for_lambda" {
   ]
 }
 EOF
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.weather.arn}"
+  principal = "apigateway.amazonaws.com"
+  source_arn = "TODO"
 }
 
 resource "aws_lambda_function" "weather" {
