8572af065f38299003447e6e8a0ecf225eee8d63
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 13:18:32 -0600

    Update method to point at zip resource

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_api_gateway_resource" "zip" {
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id      = "${aws_api_gateway_rest_api.weather_api.id}"
-  resource_id      = "${aws_api_gateway_resource.resource.id}"
+  resource_id      = "${aws_api_gateway_resource.zip.id}"
   http_method      = "ANY"
   authorization    = "NONE"
   api_key_required = true
