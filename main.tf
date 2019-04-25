44342403e0e1cec32ed580e1c2e2dd429b92c393
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 13:51:32 -0600

    Point integration at zip resource

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_api_gateway_method" "method" {
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.weather_api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  resource_id             = "${aws_api_gateway_resource.zip.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
