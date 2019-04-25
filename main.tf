a0c5ca1944a2903433f85675ab3a04efed03ef2d
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 12:50:25 -0600

    Update source arn in lambda permission

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_lambda_permission" "api_gateway" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.weather.arn}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.weather_api.execution_arn}/*/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.weather_api.execution_arn}/*/*"
 }
 
 #Lambda
