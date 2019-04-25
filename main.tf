e14d613eb7a535497358caba31f95ea8ecee6207
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 12:05:49 -0600

    Changed API method back to post

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id      = "${aws_api_gateway_rest_api.weather_api.id}"
   resource_id      = "${aws_api_gateway_resource.resource.id}"
-  http_method      = "POST"
+  http_method      = "GET"
   authorization    = "NONE"
   api_key_required = true
 }
