a05a6e6bd0046b55c50a2a3056bf2a28a228f53b
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 13:04:19 -0600

    Add zip resource

--- main.tf
+++ main.tf
@@ -23,6 +23,12 @@ resource "aws_api_gateway_resource" "resource" {
   rest_api_id = "${aws_api_gateway_rest_api.weather_api.id}"
 }
 
+resource "aws_api_gateway_resource" "zip" {
+  path_part = "{zip}"
+  parent_id = "${aws_api_gateway_resource.resource.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.weather_api.id}"
+}
+
 resource "aws_api_gateway_method" "method" {
   rest_api_id      = "${aws_api_gateway_rest_api.weather_api.id}"
   resource_id      = "${aws_api_gateway_resource.resource.id}"
