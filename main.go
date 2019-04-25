87b8bf94969a2899823e84947e6576ab18ae9138
Tim Brammer <tim.brammer@MBPR15-S04452.local>
2019-04-25 14:53:53 -0600

    Implement debug function in Lambda

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -65,6 +67,18 @@ func getWeather(zip string) (string, error) {
 	return fmt.Sprintf("Location: %s", jsonData.Name), nil
 }
 
+func debugFunc(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+}
+
 func main() {
-	lambda.Start(getWeather)
+	lambda.Start(debugFunc)
 }
